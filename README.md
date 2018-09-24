1b664f6db94ac2abb1e39417fe5bcabe6503f5bd
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-09-24 17:30:25 +0700

    Upload via URL

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 take a look at https://pythonhosted.org/PyDrive/quickstart.html
+btw you can look at the commit name, there's the thing hehehe
--- run.py
+++ run.py
@@ -6,6 +6,6 @@ gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentica
 
 drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1.SetContentString('Hello World!') # Set content of the file from given string.
+file1 = drive.CreateFile({'title': 'DistinctionBias.pdf'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1.SetContentFile("DistinctionBias.pdf") # Set content of the file from given string.
 file1.Upload()
